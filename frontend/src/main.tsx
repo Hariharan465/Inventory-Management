e491c625a571eb525b36e314e5069190708fea0a
Hariharan465 <hariharen704@gmail.com>
2025-02-19 13:55:03 +0530

    Changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,7 +13,7 @@ import "primereact/resources/themes/lara-light-cyan/theme.css";
 ReactDom.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
 
-    <PrimeReactProvider>
+    <PrimeReactProvider >
     <Provider store={store}>
     <RouterProvider router={Routes} />
     </Provider>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,15 @@
+import { InputText } from "primereact/InputText"
+
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className='min-h-screen flex items-center justify-center w-full bg-[#eee]'>
+      <div className="w-1/3 shadow-md rounded-md border-[.2px] border-black">
+      <div className="mb-3">
+        <label htmlFor="email">Email</label>
+        <InputText unstyled className='w-full outline-none py-3 px-2' placeholder="Enter Email Address"/>
+        </div>
+        </div>
     </div>
   )
 }
-
 export default Login
