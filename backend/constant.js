3e9fac2b9ac1e59c171d13571f8d22788f334f31
SGMohan <iamsgmohan@gmail.com>
2025-02-24 13:44:58 +0530

    Connection

--- backend/constant.js
+++ backend/constant.js
@@ -1,6 +1,6 @@
 class PUBLIC_DATA {
-  static port = process.env.PORT || 5000;
-  static mongo_uri = process.env.MONGO_URI || 'mongodb://localhost:27017/inventory';
+  static port = process.env.PORT || 4000;
+  static mongo_uri = process.env.MONGO_URI || `mongodb://localhost:27017/inventory`;
 } 
 module.exports = {
     PUBLIC_DATA
