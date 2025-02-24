40c4e928f65825c8e016aa4b73c370e81a2d24c2
SGMohan <iamsgmohan@gmail.com>
2025-02-24 12:55:48 +0530

    update

--- backend/constant.js
+++ backend/constant.js
@@ -1,1 +1,7 @@
-class PUBLIC_DATA =  
+class PUBLIC_DATA {
+  static port = process.env.PORT || 5000;
+  static mongo_uri = process.env.MONGO_URI || 'mongodb://localhost:27017/inventory';
+} 
+module.exports = {
+    PUBLIC_DATA
+}
--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,8 @@
+
+const { PUBLIC_DATA } = require("./constant");
 const app = require("./src/app");
 
+app.listen(PUBLIC_DATA.port, () => {
+    console.log("the app is listen at http://localhost:${PUBLIC_DATA.port}");
+}
+)
