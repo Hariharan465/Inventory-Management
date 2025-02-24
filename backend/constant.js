e20f09141e69108eff8c4060cf86f84df2c78a8f
Hariharan465 <hariharen704@gmail.com>
2025-02-24 11:38:18 +0530

    Fresh backend

--- backend/constant.js
+++ backend/constant.js
@@ -1,11 +1,1 @@
-class PUBLIC_DATA{
-
-    static port = process.env.PORT || 5000
-    static mongo_uri = process.env.MONGO_URI || `mongodb://localhost/inventry`
-
-
-}
-
-module.exports = {
-    PUBLIC_DATA
-}
+class PUBLIC_DATA =  
