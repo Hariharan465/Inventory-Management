fc82d61f79d35174b583743866b81754b8254bb2
Hariharan465 <hariharen704@gmail.com>
2025-02-24 11:41:30 +0530

    Fresh backend 1

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,2 @@
-require("dotenv").config({
+const app = require("./src/app");
 
-})
-const { PUBLIC_DATA } = require("../ ../constant");
-const app = require("./src/app"); 
-const { ConnectDB } = require("./src/config/db.config");
-ConnectDB()
-
-
-
-
-
-app.listen(PUBLIC_DATA.port,()=>{
-    console.log(`the app is listen at http://localhost:${PUBLIC_DATA.port}`);
-})
-
-
-// --views
--- backend/src/routes/Auth.route.js
+++
@@ -1,9 +0,0 @@
-const AuthController = require("../controllers/Auth.controller")
-const AuthValidation = require("../validations/Auth.validation")
-const Validation = require("../middlewares/Validation")
-
-const router  = require("express").Router()
-
-router.post("/register",AuthValidation.RegisterUser,Validation,AuthController.RegisterUser)
-
-module.exports = router
